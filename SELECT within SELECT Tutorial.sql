8f4e0c770a47f025368a9ca05d3337c9345fbdf2
BAHAR <56251068+Mohadese-ghayoomi@users.noreply.github.com>
2019-12-08 03:03:21 +0200

    Update SELECT within SELECT Tutorial.sql

--- SELECT within SELECT Tutorial.sql
+++ SELECT within SELECT Tutorial.sql
@@ -1,5 +1,6 @@
 --1.List each country name where the population is larger than that of 'Russia'.
 
+
 SELECT name
 FROM world 
 WHERE population > 
@@ -7,9 +8,17 @@ WHERE population >
 
 --2.Show the countries in Europe with a per capita GDP greater than 'United Kingdom'.
 
+
 SELECT name 
 FROM world 
 WHERE   continent ='Europe' and (GDP/population)>
 (SELECT GDP/population FROM world WHERE name='United Kingdom' )
 
---3.
+--3.List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country.
+
+
+SELECT name,continent
+FROM world
+WHERE continent IN
+(SELECT continent FROM world WHERE name IN('Argentina','Australia'))
+ORDER BY name
